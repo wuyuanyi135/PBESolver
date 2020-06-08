2e6b06ed1320d2d15b8460ad06b47ba5582df365
wuyuanyi <wuyuanyi135@gmail.com>
2020-06-08 15:52:49 +0800

    fix Simulink errors

--- solver_system.m
+++ solver_system.m
@@ -2,10 +2,15 @@ classdef solver_system < matlab.System & matlab.system.mixin.Propagates
     properties(Nontunable)
         sampleTimeSecond = 30;
         cfl = 0.1;
-        solverObj;
-        ic;
+        solverObj = [];
+        ic = [];
     end
-
+    methods
+        function obj = solver_system(varargin)
+            warning("off", "SystemBlock:MATLABSystem:ParameterWithUnspecifiedDefaultValueRestrictedToBuiltinDataType"); 
+            obj@matlab.System(varargin{:});
+        end
+    end
     methods(Access = protected)
         function setupImpl(obj)
             % Perform one-time calculations, such as computing constants
@@ -15,19 +20,22 @@ classdef solver_system < matlab.System & matlab.system.mixin.Propagates
         end
 
         function num = getNumOutputsImpl(obj)
-            % Define total number of outputs for system with optional
-            % outputs
-            num = 1;
-            num = num + numel(obj.solverObj.props);
+            num = 1; % concentration
+            % csd
+            if ~isempty(obj.solverObj)
+                num = num + numel(obj.solverObj.props);
+            end
         end
 
         function [c, varargout] = getOutputNamesImpl(obj)
             % Return output port names for System block
             c = 'c';
-            n = numel(obj.solverObj.props);
-            varargout = cell(n, 1);
-            for i = 1 : n
-                varargout{i} = ['csd' num2str(i)];
+            if ~isempty(obj.solverObj)
+                n = numel(obj.solverObj.props);
+                varargout = cell(n, 1);
+                for i = 1 : n
+                    varargout{i} = ['csd' num2str(i)];
+                end
             end
         end
 
@@ -37,40 +45,43 @@ classdef solver_system < matlab.System & matlab.system.mixin.Propagates
             in = make_inputs(u);
             nextState = obj.solverObj.step([], in, obj.cfl, obj.sampleTimeSecond);
             c = nextState(1).conc;
-%             n = numel(obj.solverObj.props);
-%             varargout = cell(n, 1);
-%             for i = 1 : n
-%                 varargout{i} = nextState(i).csd;
-%             end
             varargout = {nextState.csd};
         end
 
         function [c, varargout] = getOutputSizeImpl(obj)
             c = [1 1];
-            n = numel(obj.solverObj.props);
-            varargout = cell(n, 1);
-            for i = 1 : n
-                varargout{i} = [obj.solverObj.props(1).sizeGrids.nPoints 1];
+            if ~isempty(obj.solverObj)
+                n = numel(obj.solverObj.props);
+                varargout = cell(n, 1);
+                for i = 1 : n
+                    varargout{i} = [obj.solverObj.props(1).sizeGrids.nPoints 1];
+                end
             end
         end
 
         function [c, varargout] = getOutputDataTypeImpl(obj)
             c = "double";
-            n = numel(obj.solverObj.props);
-            sa = repmat("double", n, 1);
-            varargout = sa.cellstr;
+            if ~isempty(obj.solverObj)
+                n = numel(obj.solverObj.props);
+                sa = repmat("double", n, 1);
+                varargout = sa.cellstr;
+            end
         end
 
         function [c, varargout] = isOutputComplexImpl(obj)
             c = false;
-            n = numel(obj.solverObj.props);
-            varargout = repmat({false}, n, 1);
+            if ~isempty(obj.solverObj)
+                n = numel(obj.solverObj.props);
+                varargout = repmat({false}, n, 1);
+            end
         end
 
         function [c, varargout] = isOutputFixedSizeImpl(obj)
             c = true;
-            n = numel(obj.solverObj.props);
-            varargout = repmat({true}, n, 1);
+            if ~isempty(obj.solverObj)
+                n = numel(obj.solverObj.props);
+                varargout = repmat({true}, n, 1);
+            end
         end
     end
 end
